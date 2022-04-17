6d25f2b009fd71005c91a01ca4fdf2c589374da0
Aadi Poddar <aadipoddarmail@gmail.com>
2022-04-17 18:33:04 +0530

    Header

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Header from '../components/Header'
 
 export default function Home() {
   return (
@@ -9,9 +10,7 @@ export default function Home() {
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <h1 className='text-3xl font-bold underline'>
-        Google Docs!
-      </h1>
+      <Header />
     </div>
   )
 }
