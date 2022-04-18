8208df05425076920bf58a34963e1f93ea625ff3
Aadi Poddar <aadipoddarmail@gmail.com>
2022-04-18 17:10:19 +0530

    Text Editor

--- pages/doc/[id].js
+++ pages/doc/[id].js
@@ -1,10 +1,12 @@
+import Head from 'next/Head'
 import Button from '@material-tailwind/react/Button'
 import Icon from '@material-tailwind/react/Icon'
 import { useRouter } from 'next/dist/client/router'
-import { db } from '../../firebase'
 import { useDocumentOnce } from 'react-firebase-hooks/firestore'
 import { getSession, signOut, useSession } from 'next-auth/client'
+import { db } from '../../firebase'
 import Login from '../../components/Login'
+import TextEditor from '../../components/TextEditor'
 
 export default function doc() {
     const router = useRouter()
@@ -19,45 +21,59 @@ export default function doc() {
 
     return (
         <div>
+            <Head>
+                <title>{snapshot?.data()?.fileName}</title>
+            </Head>
+
             <header className='flex justify-between items-center p-3 pb-1'>
                 <span className='cursor-pointer' onClick={() => router.push('/')}>
                     <Icon name='description' size='5xl' color='blue' />
                 </span>
 
-                <div className="flex-grow px-2">
+                <div className='flex-grow px-2'>
                     <h2>{snapshot?.data()?.fileName}</h2>
-                    <div className="flex items-center text-sm space-x-1 -ml-1 h-8 text-gray-600">
-                        <p className="option">File</p>
-                        <p className="option">Edit</p>
-                        <p className="option">View</p>
-                        <p className="option">Insert</p>
-                        <p className="option">Format</p>
-                        <p className="option">Tools</p>
+                    <div className='flex items-center text-sm space-x-1 -ml-1 h-8 text-gray-600'>
+                        <p className='option'>File</p>
+                        <p className='option'>Edit</p>
+                        <p className='option'>View</p>
+                        <p className='option'>Insert</p>
+                        <p className='option'>Format</p>
+                        <p className='option'>Tools</p>
                     </div>
                 </div>
 
                 <Button
-                    color="lightBlue"
-                    buttonType="filled"
-                    size="regular"
+                    color='lightBlue'
+                    buttonType='filled'
+                    size='regular'
                     rounded={false}
                     iconOnly={false}
                     block={false}
-                    ripple="light"
-                    className="hidden md:inline-flex h-10"
+                    ripple='light'
+                    className='hidden md:inline-flex h-10'
                 >
-                    <Icon name="people" size="3xl" />
+                    <Icon name='people' size='3xl' />
                     SHARE
                 </Button>
 
                 <img
-                    loading="lazy"
-                    className="cursor-pointer h-12 w-12 rounded-full ml-2"
+                    loading='lazy'
+                    className='cursor-pointer h-12 w-12 rounded-full ml-2'
                     onClick={signOut}
                     src={session?.user?.image}
-                    alt=""
+                    alt=''
                 />
             </header>
+
+            <TextEditor />
         </div>
     )
+}
+
+export async function getServerSideProps(context) {
+    const session = await getSession(context)
+
+    return {
+        props: { session }
+    }
 }
